c5d24a7e0a68d0de853f90d2ffd635c7e2cf0144
swapnilpangudwale <163035147+swapnilpangudwale@users.noreply.github.com>
2025-02-01 20:23:42 +0530

    Update TestCases.java

--- SeleniumPracticleTest/src/test/java/testCases/Scenario1/TestCases.java
+++ SeleniumPracticleTest/src/test/java/testCases/Scenario1/TestCases.java
@@ -21,32 +21,27 @@ public class TestCases extends BaseTest {
 	public void tc_01_ValidateTextMessage() throws InterruptedException {
 
 		WebDriverWait wait = new WebDriverWait(driver, 30);
-		Thread.sleep(1000);
 		WebElement simpleFormDemo_Link = driver.findElement(By.xpath(locator.getProperty("simpleFormDemo_Link")));
 		wait.until(ExpectedConditions.elementToBeClickable(simpleFormDemo_Link));
 		simpleFormDemo_Link.click();
 
-		Thread.sleep(1000);
+	
 		String actualURL = driver.getCurrentUrl();
 		wait.until(ExpectedConditions.urlContains("simple-form-demo"));
 		Assert.assertTrue(actualURL.contains("simple-form-demo"));
-
-		Thread.sleep(1000);
+		
 		WebElement enterMessage = driver.findElement(By.xpath(locator.getProperty("enterMessage_TxtBox")));
 		wait.until(ExpectedConditions.visibilityOf(enterMessage));
 		enterMessage.sendKeys(prop.getProperty("message"));
 
-		Thread.sleep(1000);
 		WebElement clickOnGetCheckedValue = driver.findElement(By.id(locator.getProperty("getCheckedValue_Button")));
 		wait.until(ExpectedConditions.elementToBeClickable(clickOnGetCheckedValue));
 		clickOnGetCheckedValue.click();
 
-		Thread.sleep(1000);
 		WebElement msg = driver.findElement(By.xpath(locator.getProperty("actualMessage")));
 		wait.until(ExpectedConditions.visibilityOf(enterMessage));
 		String getActualMsg = msg.getText();
 
-		Thread.sleep(1000);
 		Assert.assertEquals(prop.getProperty("message"), getActualMsg);
 
 	}
