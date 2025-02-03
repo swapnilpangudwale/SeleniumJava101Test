ba48f95b2a424b542e70d1dd1811dc4229354223
swapnilpangudwale <163035147+swapnilpangudwale@users.noreply.github.com>
2025-02-03 21:38:16 +0530

    Update TestCase2.java

--- SeleniumPracticleTest/src/test/java/testCases/Scenario1/Scenario2/TestCase2.java
+++ SeleniumPracticleTest/src/test/java/testCases/Scenario1/Scenario2/TestCase2.java
@@ -19,23 +19,20 @@ public class TestCase2 extends BaseTest {
 
 		WebDriverWait wait = new WebDriverWait(driver, 30);
 
-		Thread.sleep(1000);
 		WebElement clickOnLink = driver.findElement(By.xpath(locator.getProperty("dragAndDrop_Link")));
 		wait.until(ExpectedConditions.elementToBeClickable(clickOnLink));
 		clickOnLink.click();
 
-		Thread.sleep(1000);
 		WebElement slider15 = driver.findElement(By.xpath(locator.getProperty("defalutValue15_Slider")));
 		wait.until(ExpectedConditions.visibilityOf(slider15));
 
 		
 		Actions slider = new Actions(driver);
 		slider.clickAndHold(slider15);
-	Thread.sleep(1000);
 		slider.moveByOffset(212, 0).build().perform();
 
 		WebElement sliderValue = driver.findElement(By.xpath(locator.getProperty("defaultValue15_SliderValue")));
-		Thread.sleep(1000);
+	
 		wait.until(ExpectedConditions.visibilityOf(sliderValue));
 		String getActualSliderValue = sliderValue.getText();
 		Assert.assertEquals(Integer.parseInt(getActualSliderValue), 95);
